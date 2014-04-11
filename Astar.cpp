b0ffdba5de86a643d5126d1127cfafb8e2000d49
Andrew <avaziri3@gatech.edu>
2014-04-10 22:14:10 -0400

    Adding main file as hello world example

---
+++ Astar.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Astar.cpp
+// Author      : Andrew Vaziri
+// Version     :
+// Copyright   : none
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!! ?" << endl; // prints !!!Hello World!!! ?
+	return 0;
+}
