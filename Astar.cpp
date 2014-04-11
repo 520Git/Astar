e7bbf36f786e19880837b0e1d9aa63ddf7601bf3
Andrew <avaziri3@gatech.edu>
2014-04-10 22:54:50 -0400

    changes Astar to important node.h and use a function in node.cpp as a test

--- Astar.cpp
+++ Astar.cpp
@@ -3,13 +3,25 @@
 // Author      : Andrew Vaziri
 // Version     :
 // Copyright   : none
-// Description : Hello World in C++, Ansi-style
+// Description : A* Search
 //============================================================================
 
 #include <iostream>
+#include <queue>
+#include "node.h"
 using namespace std;
+
+const int n=60;   //Initialize dimensions and map
+const int m=80;
+int map[n][m];
+//int closed_nodes[n][m];
+//int dir_map[n][m];
+std::priority_queue<int> open_nodes;  	//Initialize a priority queue for the open nodes
+
+
 
 int main() {
 	cout << "!!!Hello World!!! ?" << endl; // prints !!!Hello World!!! ?
+	cout << add(n,m) << endl;
 	return 0;
 }
