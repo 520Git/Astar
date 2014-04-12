ed6200fc5f3b58f88855619300867e6f4f3ad88c
Andrew <avaziri3@gatech.edu>
2014-04-12 15:53:17 -0400

    Changes dimensions from constant ints to #defines, implemented neighbor structure in map nodes

--- node.cpp
+++ node.cpp
@@ -6,6 +6,9 @@
  */
 #include "node.h"
 #include <cstring>
+#include <iostream>
+#include <limits>
+using namespace std;
 
 
 //Test function to check my ability to change the values of a map_node
@@ -16,18 +19,63 @@ void map_node::init(int x, int y, bool z){
 	return;
 };
 
+//Function to initialize the pointers to neighbors so I can traverse the nodes like a graph
+//Rather than rely on the simple case of a 2d array
+void map_node::initNeighbors(int rows, int cols, map_node* map){
+	//Give all nodes four neighbors, nodes on edges have null pointer neighbors
+	if((yPos-1)==-1)neighbors[0]=0;
+	else neighbors[0]=&map[xyc(xPos,(yPos-1))];    //above
+	if((xPos+1)==cols)neighbors[1]=0;
+	else neighbors[1]=&map[xyc((xPos+1),yPos)];    //right
+	if((yPos+1)==rows) neighbors[2]=0;
+	else neighbors[2]=&map[xyc(xPos,(yPos+1))];    //below
+	if((xPos-1)==-1)neighbors[3]=0;
+	else neighbors[3]=&map[xyc((xPos-1),yPos)];         //left
+	return ;
+};
 
-//Function to test my ability to initialize an array of nodes.
-//In the future this would initialize some of them randomly
-//as obstructed
+
+//Function to set initial values of all nodes in the map
 map_node* initMap(int rows, int cols){
 	size_t s = sizeof(map_node)*rows*cols;
 	map_node* map = new map_node[s];
 	for(int i=0; i<rows; i++){
 		for(int j=0; j<cols; j++){
-			map[i*cols+j].init(0,0,0);
+			map[i*cols+j].xPos=j;
+			map[i*cols+j].yPos=i;
+			map[i*cols+j].gScore = 0;
+			map[i*cols+j].fScore = 999999999;
+			map[i*cols+j].initNeighbors(rows, cols, map);
 		}
 	}
 	return map;
 }
+
+//Function I sometimes use in debugging to prompt user and wait for enter key to be pressed
+void wait()
+  {
+  std::cout << "Press ENTER to continue...";
+  std::cin.ignore( std::numeric_limits <std::streamsize> ::max(), '\n' );
+  }
+
+//Prints out all of the nodes, their addresses and the address of their neighbors
+void debugNeighbors(map_node* map){
+	for(int y=0;y<NUMROWS;y++){
+			for(int x=0;x<NUMCOLS;x++){
+				//Print out current node
+				cout<< '(' << x << ',' << y << "):" << '[' << map[xyc(x,y)].xPos << ',' << map[xyc(x,y)].yPos << ']' << "with address: "<< &map[xyc(x,y)] << endl;
+				for(int q=0; q<4; q++){
+					//Print null if there is no neighbor in a certain position
+					if(map[xyc(x,y)].neighbors[q]==0){
+						cout<< "   Has Neighbors #" << q << ": NULL" << endl;
+						continue;
+					}
+					//Otherwise print the neighbors x,y location and address
+					cout<< "   Has Neighbors #" << q << ": [" << map[xyc(x,y)].neighbors[q]->xPos << ',' << map[xyc(x,y)].neighbors[q]->yPos << ']' << "with address: "<< map[xyc(x,y)].neighbors[q] << endl;
+				}
+				//wait();
+			}
+		}
+}
+
 
--- node.h
+++ node.h
@@ -8,42 +8,28 @@
 #ifndef NODE_H_
 #define NODE_H_
 
+#define NUMROWS 40
+#define NUMCOLS 500 //Make sure to change the companion #define in the next file
+
+#define ijc(i,j) (i*NUMCOLS+j)
+#define xyc(x,y) (y*NUMCOLS+x)
+
 int add(int x, int y);
 
 class map_node {
-	int xPos;
-	int yPos;
-	int gScore;
-	int fScore;
+
+public:
+	int xPos, yPos,gScore,fScore;
 	bool isObstructed;
 	bool isClosed=0;
-	//mapNode* neighbors;
-public:
+	map_node* neighbors[4];
 	void init(int xPos, int yPos, bool isClosed);
 	void calculateGScore(map_node start); //Not yet implemented
 	void calculateFScore(map_node start, int (*Hueristic)(map_node)); //Not yet implemented
+	void initNeighbors(int rows, int cols, map_node* map);
 };
 
 map_node* initMap(int rows, int cols);
-
-
-
-
-//Previously I tried using a struct (as can be seen below), I abandoned it after not being
-//able to find a sensible way to initialize a 2-D array of structs. Using a class instead
-//of a struct has additional advantages. For example: if I want to solve a graph where each
-//node has an arbitrary and different number of neighbors keeping them in a 2D array would not
-//be appropriate. As such it will be nice to have a class with functions rather than
-//a struct with a bunch of functions declared separately. Still I dont know if this was the right
-//choice.
-
-//struct map_node {
-//	int xPos;
-//	int yPos;
-//	int gScore;
-//	int fScore;
-//	bool isClosed=0;
-//	map_node* neighbors;
-//};
+void debugNeighbors(map_node* map);
 
 #endif /* NODE_H_ */
